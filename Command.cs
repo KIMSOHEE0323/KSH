0a40591c08fb00bfb259662e05ecee2b811da00a
unknown <qptsohee@gmail.com>
2026-09-21 12:35:43 +0900

    연습중

---
+++ Command.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.ApplicationServices;
+
+
+
+namespace KSH2
+{
+    [Transaction(TransactionMode.Manual)]
+
+    public class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // 깃허브에 올리기 연습
+            TaskDialog.Show("KSH", "Hello, Revit!");
+            return Result.Succeeded;
+        }
+    }
+}
