3d99bcb5b2a92a93ea520d6dfeec4825e80965f3
unknown <qptsohee@gmail.com>
2026-09-21 15:09:55 +0900

    루푸구문 까지 공부함

--- Command.cs
+++ Command.cs
@@ -1,25 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using Autodesk.Revit.Attributes;
-using Autodesk.Revit.ApplicationServices;
-
-
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using static Autodesk.Revit.DB.SpecTypeId;
 
 namespace KSH2
 {
     [Transaction(TransactionMode.Manual)]
-
     public class Command : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            // 깃허브에 올리기 연습
-            TaskDialog.Show("KSH", "Hello, Revit!");
+            string result = "Hello, Revit!";
+            int count = 0;
+            double sum = 0.0;
+            bool isactive = true;
+
+            //List<string> list = new List<string>();
+            //list.Add("대한");
+            //list.Add("민국");
+            //list.Add("만세");
+
+
+            for (int i = 1; i < 10; i+=2)
+            {
+                Debug.Print(i.ToString());
+            }           
+
+
+            //foreach (string a in list)
+            //{
+            //    foreach (string b in list)
+            //    {
+            //        Debug.Print(a + "@" + b);
+            //    }
+            //}
+
+            //List<string> list1 = new List<string>();
+            //list1.Add("대한AA");
+            //list1.Add("민국BB");
+            //list1.Add("만세CC");
+
+
+            //string resultA = list[0] + list1[0];
+            //string resultB = list[1] + list1[1];
+            //string resultC = list[2] + list1[2];
+
+            //Debug.Print(resultA);
+            //Debug.Print(resultB);
+            //Debug.Print(resultC);
+
             return Result.Succeeded;
         }
     }
--- obj/Debug/net8.0/KSH2.AssemblyInfo.cs
+++ obj/Debug/net8.0/KSH2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KSH2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80f72d5eb443216a360340c65daabbc385823220")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a40591c08fb00bfb259662e05ecee2b811da00a")]
 [assembly: System.Reflection.AssemblyProductAttribute("KSH2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KSH2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
